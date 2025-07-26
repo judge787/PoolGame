f0c000dd8d704f7e72b068428dae0bf6c913ef4a
judge787 <jashanjudge87@gmail.com>
2025-07-26 14:31:30 -0400

    changed ball design

--- server.py
+++ server.py
@@ -17,6 +17,109 @@ scorePlayerTwo = 0
 p1Play = ""
 p2Play = ""
 lastTable = None
+currentPlayer = 1  # 1 for player one, 2 for player two
+player1Type = None  # Will be "solids" or "stripes" 
+player2Type = None  # Will be "solids" or "stripes"
+
+def checkBallsPocketed(beforeTable, afterTable):
+    """Check which balls were pocketed by comparing before and after tables"""
+    beforeBalls = set()
+    afterBalls = set()
+    
+    # Get ball numbers from before table
+    for ball in beforeTable:
+        if isinstance(ball, (Physics.StillBall, Physics.RollingBall)):
+            if isinstance(ball, Physics.StillBall):
+                beforeBalls.add(ball.obj.still_ball.number)
+            else:
+                beforeBalls.add(ball.obj.rolling_ball.number)
+    
+    # Get ball numbers from after table  
+    for ball in afterTable:
+        if isinstance(ball, (Physics.StillBall, Physics.RollingBall)):
+            if isinstance(ball, Physics.StillBall):
+                afterBalls.add(ball.obj.still_ball.number)
+            else:
+                afterBalls.add(ball.obj.rolling_ball.number)
+    
+    # Return balls that were pocketed (in before but not in after)
+    return beforeBalls - afterBalls
+
+def updatePlayerTypes(pocketedBalls):
+    """Assign player types (solids/stripes) based on first ball pocketed"""
+    global player1Type, player2Type, currentPlayer
+    
+    # Skip cue ball and 8-ball for type assignment
+    validBalls = [ball for ball in pocketedBalls if ball != 0 and ball != 8]
+    
+    if validBalls and player1Type is None and player2Type is None:
+        firstBall = validBalls[0]
+        if 1 <= firstBall <= 7:  # Solid ball pocketed
+            if currentPlayer == 1:
+                player1Type = "solids"
+                player2Type = "stripes"
+            else:
+                player1Type = "stripes" 
+                player2Type = "solids"
+        elif 9 <= firstBall <= 15:  # Stripe ball pocketed
+            if currentPlayer == 1:
+                player1Type = "stripes"
+                player2Type = "solids"
+            else:
+                player1Type = "solids"
+                player2Type = "stripes"
+
+def updateScores(pocketedBalls):
+    """Update player scores based on pocketed balls"""
+    global scorePlayerOne, scorePlayerTwo, player1Type, player2Type
+    
+    for ball in pocketedBalls:
+        if ball == 0:  # Cue ball - no points
+            continue
+        elif ball == 8:  # 8-ball - special handling needed
+            continue
+        elif 1 <= ball <= 7:  # Solid balls
+            if player1Type == "solids":
+                scorePlayerOne += 1
+            elif player2Type == "solids":
+                scorePlayerTwo += 1
+        elif 9 <= ball <= 15:  # Stripe balls
+            if player1Type == "stripes":
+                scorePlayerOne += 1
+            elif player2Type == "stripes":
+                scorePlayerTwo += 1
+
+def shouldSwitchTurns(pocketedBalls):
+    """Determine if turn should switch based on game rules"""
+    global currentPlayer, player1Type, player2Type
+    
+    # If cue ball was pocketed, switch turns (scratch)
+    if 0 in pocketedBalls:
+        return True
+    
+    # If no balls were pocketed, switch turns
+    if not pocketedBalls or all(ball in [0, 8] for ball in pocketedBalls):
+        return True
+    
+    # If player hasn't been assigned a type yet, don't switch if they pocketed a ball
+    if player1Type is None and player2Type is None:
+        return False
+    
+    # Check if player pocketed their own balls
+    playerType = player1Type if currentPlayer == 1 else player2Type
+    validBalls = [ball for ball in pocketedBalls if ball != 0 and ball != 8]
+    
+    if playerType == "solids":
+        ownBalls = [ball for ball in validBalls if 1 <= ball <= 7]
+    else:  # stripes
+        ownBalls = [ball for ball in validBalls if 9 <= ball <= 15]
+    
+    # If they pocketed their own balls, they continue
+    if ownBalls:
+        return False
+    
+    # Otherwise, switch turns
+    return True
 
 def setupTable():
     table = Physics.Table()
@@ -69,16 +172,30 @@ class RequestHandler(SimpleHTTPRequestHandler):
             
             response = dict(parse_qsl(requestPath.query))
             id = int(response.get("id"))
+            
+            # Determine current player name and status
+            currentPlayerName = gameInstance.player1Name if currentPlayer == 1 else gameInstance.player2Name
+            
+            # Determine player statuses based on turn
+            if currentPlayer == 1:
+                p1Status = f"{gameInstance.player1Name}'s Turn"
+                p2Status = ""
+            else:
+                p1Status = ""
+                p2Status = f"{gameInstance.player2Name}'s Turn"
 
             gameData = {
-                "p1Play": p1Play,
-                "p2Play": p2Play,
+                "p1Play": p1Status,
+                "p2Play": p2Status,
                 "p1Name": gameInstance.player1Name,
                 "p2Name": gameInstance.player2Name,
                 "p1Score": scorePlayerOne,
                 "p2Score": scorePlayerTwo,
                 "svg": lastTable.svg(),
-                "gameName": gameInstance.gameName
+                "gameName": gameInstance.gameName,
+                "currentPlayer": currentPlayerName,
+                "p1Type": player1Type or "Unassigned",
+                "p2Type": player2Type or "Unassigned"
             }
 
             response = json.dumps(gameData)
@@ -103,7 +220,7 @@ class RequestHandler(SimpleHTTPRequestHandler):
     def do_POST(self):
         requestPath = urlparse(self.path)
 
-        global gameInstance, lastTable, activePlayer  # Use global declarations for modifications
+        global gameInstance, lastTable, activePlayer, currentPlayer, scorePlayerOne, scorePlayerTwo, player1Type, player2Type  # Use global declarations for modifications
         
         if requestPath.path == '/start.html':
             # Processing form data to start a new game
@@ -115,9 +232,16 @@ class RequestHandler(SimpleHTTPRequestHandler):
             playerTwoName = formData.getvalue("secondPlayer")
             gameName = formData.getvalue("gameName")
             
+            # Reset game state
+            global currentPlayer, scorePlayerOne, scorePlayerTwo, player1Type, player2Type
+            currentPlayer = 1  # Start with player 1
+            scorePlayerOne = 0
+            scorePlayerTwo = 0
+            player1Type = None
+            player2Type = None
 
             gameInstance = Physics.Game(gameName=gameName, player1Name=playerOneName, player2Name=playerTwoName)
-            activePlayer = random.choice([playerOneName, playerTwoName])
+            activePlayer = playerOneName  # Start with player 1
             lastTable = setupTable()
 
             with open("start.html", "r") as file:
@@ -145,11 +269,33 @@ class RequestHandler(SimpleHTTPRequestHandler):
             velocityX = float(formData.getvalue('velocityX'))
             velocityY = float(formData.getvalue('velocityY'))
             
-            lastTable, tableList = gameInstance.shoot(gameInstance.gameName, activePlayer, lastTable, velocityX, velocityY)
+            # Store the table state before the shot
+            beforeTable = lastTable
+            
+            # Execute the shot
+            currentPlayerName = gameInstance.player1Name if currentPlayer == 1 else gameInstance.player2Name
+            lastTable, tableList = gameInstance.shoot(gameInstance.gameName, currentPlayerName, lastTable, velocityX, velocityY)
+            
+            # Check which balls were pocketed
+            pocketedBalls = checkBallsPocketed(beforeTable, lastTable)
+            
+            # Update player types if this is the first ball pocketed
+            updatePlayerTypes(pocketedBalls)
+            
+            # Update scores
+            updateScores(pocketedBalls)
+            
+            # Determine if turn should switch
+            if shouldSwitchTurns(pocketedBalls):
+                currentPlayer = 2 if currentPlayer == 1 else 1
+                activePlayer = gameInstance.player2Name if currentPlayer == 2 else gameInstance.player1Name
 
             tableList = json.dumps(tableList)
 
-            print("Called shoot")
+            print(f"Called shoot - Player {currentPlayer} ({currentPlayerName})")
+            print(f"Balls pocketed: {pocketedBalls}")
+            print(f"Score P1: {scorePlayerOne}, Score P2: {scorePlayerTwo}")
+            print(f"Player types - P1: {player1Type}, P2: {player2Type}")
 
             self.send_response(200)
             self.send_header("Content-type", "application/json")
